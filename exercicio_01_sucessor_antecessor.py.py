f59daae698ca40058b90f56acb871d297c92df7b
Leticia Giovanna Lopez <leticiagiovannad@gmail.com>
2025-12-23 13:01:34 -0300

    adiciona exercício 01 (sucessor e antecessor)

---
+++ exercicio_01_sucessor_antecessor.py.py
@@ -0,0 +1,9 @@
+n = int(input('Escolha um número: '))
+
+sucessor = n + 1
+antecessor = n - 1
+
+print(
+    f'Analisando o número {n}, seu sucessor é {sucessor}, '
+    f'e seu antecessor é {antecessor}.'
+)
