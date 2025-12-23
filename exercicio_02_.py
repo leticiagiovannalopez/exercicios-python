8049e135e68d5246e8db75bb2e1add8e948386c5
Leticia Giovanna Lopez <leticiagiovannad@gmail.com>
2025-12-23 13:04:35 -0300

    adiciona exercício 02

---
+++ exercicio_02_.py
@@ -0,0 +1,9 @@
+n = int(input('escolha um número: '))
+d = n * 2 
+t = n * 3
+rq = n ** (1/2)
+
+print(
+    f'analisando o número {n}, vemos que o dobro do mesmo corresponde a {d}, '
+    f'seu triplo vale {t}, e sua raiz quadrada é igual a {rq:.2f}'
+)
