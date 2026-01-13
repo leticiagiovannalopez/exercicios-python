6e0dad40e056cb34387f6a9b23c693bd9d50d348
Leticia Giovanna Lopez <leticiagiovannad@gmail.com>
2026-01-13 18:49:47 -0300

    corrige exercicio 17

--- exercicio_16.py
+++ exercicio_16.py
@@ -1,8 +1,6 @@
-import random 
-apresentação = [input('primeiro aluno: '),
-                input('segundo aluno: '),
-                input('terceiro aluno: '),
-                input('quarto aluno: '),
-                input('quinto aluno: ')]
-
-print(f'a ordem escolhida dos alunos para apresentação do trabalho é: {random.shuffle(apresentação)} ')
+preço = float(input('qual é o valor do produto?: '))
+desconto = preço * 10 / 100  
+acrescimo = preço * 8 / 100 
+preço_a_vista = preço - desconto
+preço_juros = preço + acrescimo
+print(f'um produto que custa R${preço}, terá um desconto de R${desconto}, totalizando R${preço_a_vista} se for pago à vista, e um acréscimo de R${acrescimo} se for no cartão de crédito, que totaliza R${preço_juros}')
